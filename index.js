eb4116f8d426acf68b8e6f4a6d4124b100ac3f87
Joan <joanmuhingo@gmail.com>
2023-08-03 18:55:05 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+const main = document.querySelector('main');
+main.remove();
+
+
+const newHeader = document.createElement("h1")
+document.body.append(newHeader);
+newHeader.nodeName = "h1";
+newHeader.id ="victory";
+newHeader.innerHTML='JOJO is the champion';
+
+
+
+
